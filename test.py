82d7b7440848aaa9e44953c794ec6edf67e697bb
kmurp62rulz <kmurp62rulz@gmail.com>
2022-08-10 19:36:45 +1000

    updates for 22/23 season

--- test.py
+++ test.py
@@ -9,26 +9,16 @@ response = requests.get(link)
 
 data = json.loads(response.text)
 
-
-def gameweeknum():
-    num = data['standings']['results'][0]['matches_played']
-    return str(num)
-
-
-
 def laddertweet():
     tweet = ""
     for i in data['standings']['results']:
         position = ""
-        if i['last_rank'] != 0:
-            if i['rank'] < i['last_rank']:
-                position = "⬆️"
-            elif i['rank'] > i['last_rank']:
-                position = "⬇️"
+        if i['rank'] < i['last_rank']:
+            position = "⬆️"
+        elif i['rank'] > i['last_rank']:
+            position = "⬇️"
         entry = str(i['rank']) + '. ' + i['entry_name'] + ' - ' + str(i['total']) + position + '\n'
         tweet += entry
     return tweet
 
-
-
-
+print(laddertweet())
